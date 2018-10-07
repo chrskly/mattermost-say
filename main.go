dc2b9a6f0f876d12558748d2689673a1b3958261
Christian Kelly <ckelly@afilias.info>
2018-10-07 18:03:53 +0100

    Pass input to say fn

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ package main
 import (
     "fmt"
     "stdinreader"
+
+    "mmsay"
 )
 
 func main() {
     input, err := stdinreader.Read()
     _ = err
-    fmt.Println(input)
+    //fmt.Println(input)
+
+    resp, err := mmsay.Say(input)
+    _ = resp
+    if err != nil {
+        fmt.Errorf("could not post message: %v", err)
+    }
 }
